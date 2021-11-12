0da822e57809295500936c94f33e26a256b9aee1
Blake Rhoden <zbrhoden@gmail.com>
2021-11-12 15:23:54 -0600

    ch 12

--- levelup/urls.py
+++ levelup/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user, GameType, Game
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView, EventView
+from levelupapi.views import GameTypeView, GameView, EventView, user_profile
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'events', EventView, 'event')
 router.register(r'game', GameView, 'game')
 
 
@@ -16,6 +17,7 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('profile', user_profile),
 ]
 
 
